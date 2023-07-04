24f08b6470e73e8ab006fde737f75069e3256e18
Franklin Koch <franklinwkoch@gmail.com>
2023-07-04 17:43:39 -0600

    🙊 Suppress failure to load external entity warning in meca validation

--- packages/jats-xml/src/session.ts
+++ packages/jats-xml/src/session.ts
@@ -2,10 +2,22 @@ import { chalkLogger, LogLevel } from 'myst-cli-utils';
 import type { Logger } from 'myst-cli-utils';
 import type { ISession } from './types.js';
 
+function xmllintLogWrapper(logger: Logger): Logger {
+  return {
+    ...logger,
+    error(data: string) {
+      const line = data.trim();
+      if (!line) return;
+      if (line.includes('warning: failed to load external entity')) return;
+      logger.error(data);
+    },
+  };
+}
+
 export class Session implements ISession {
   log: Logger;
   constructor(opts?: { logger?: Logger }) {
-    this.log = opts?.logger ?? chalkLogger(LogLevel.debug);
+    this.log = xmllintLogWrapper(opts?.logger ?? chalkLogger(LogLevel.debug));
   }
 }
 
