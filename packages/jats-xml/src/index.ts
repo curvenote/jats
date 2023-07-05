40f42d16fa724f2d1dd601466e9afe6e434f9a78
Franklin Koch <franklinwkoch@gmail.com>
2023-07-05 12:30:56 -0600

    🙊 Suppress failure to load external entity warning with xmllint (#14)

--- packages/jats-xml/src/index.ts
+++ packages/jats-xml/src/index.ts
@@ -1,4 +1,5 @@
 export { default as version } from './version.js';
 export { Jats } from './jats.js';
+export * from './session.js';
 export * from './types.js';
 export * from './validate/index.js';
