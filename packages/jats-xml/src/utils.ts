a53ae84b8019ea85941b2ff3909f0a857ffcf6aa
Franklin Koch <franklinwkoch@gmail.com>
2024-09-08 22:34:37 -0600

    🏫 Improve affiliation handling

--- packages/jats-xml/src/utils.ts
+++ packages/jats-xml/src/utils.ts
@@ -1,10 +1,12 @@
-import type { GenericParent } from 'myst-common';
+import type { GenericNode, GenericParent } from 'myst-common';
 import { toText } from 'myst-common';
+import type { Node } from 'myst-spec';
 import { doi } from 'doi-utils';
 import { select, selectAll } from 'unist-util-select';
-import type { Affiliation, ArticleId, Xref } from 'jats-tags';
+import type { Affiliation, ArticleId, Contrib, Xref } from 'jats-tags';
 import { Tags } from 'jats-tags';
-import type { Contributor } from 'myst-frontmatter';
+import type { Affiliation as AffiliationFM, Contributor as ContributorFM } from 'myst-frontmatter';
+import { remove } from 'unist-util-remove';
 
 export type PubIdTypes = 'doi' | 'pmc' | 'pmid' | 'publisher-id' | string;
 
@@ -21,45 +23,103 @@ export function findArticleId(
   return toText(doiTag) || undefined;
 }
 
-export function authorAndAffiliation(node: GenericParent, article: GenericParent): Contributor {
-  const author: Contributor = {
-    name: `${toText(select(Tags.givenNames, node))} ${toText(select(Tags.surname, node))}`,
+export function processContributor(contrib: Contrib): ContributorFM {
+  const author: ContributorFM = {
+    name: `${toText(select(Tags.givenNames, contrib))} ${toText(select(Tags.surname, contrib))}`,
   };
-  const orcid = select('[contrib-id-type=orcid]', node);
+  const orcid = select('[contrib-id-type=orcid]', contrib);
   if (orcid) {
     author.orcid = toText(orcid).replace(/(https?:\/\/)?orcid\.org\//, '');
   }
-  //
-  /**
-   * For example:
-   *
-   * ```xml
-   * <aff id="aff2">
-   * <label>2</label>
-   * <institution-wrap>
-   * <institution-id institution-id-type="ror">https://ror.org/00t9vx427</institution-id>
-   * <institution>Department of Biochemistry, University of Texas Southwestern Medical Center</institution>
-   * </institution-wrap>
-   * <addr-line>
-   * <named-content content-type="city">Dallas</named-content>
-   * </addr-line>
-   * <country>United States</country>
-   * </aff>
-   * ```
-   */
-  const affiliationRefs = selectAll('xref[ref-type=aff]', node) as Xref[];
-  const affiliations = affiliationRefs.map((xref) =>
-    select(`[id=${xref.rid}]`, article),
-  ) as Affiliation[];
-  const affiliationText = affiliations
-    .map((aff) => {
-      // TODO: handle rors!
-      const ror = select(`[institution-id-type=ror]`, aff);
-      return toText(select('institution', aff));
-    })
-    .filter((t) => !!t);
-  if (affiliationText.length > 0) {
-    author.affiliations = affiliationText;
+  const affiliationRefs = selectAll('xref[ref-type=aff]', contrib) as Xref[];
+  const affiliationIds = affiliationRefs.map((xref) => xref.rid);
+  if (affiliationIds.length > 0) {
+    author.affiliations = affiliationIds;
   }
+  // If there are no aff xrefs AND contrib is in a contrib group with affs AND those affs do not have IDs, add them as affiliations...
   return author;
+}
+
+/**
+ * Perform standard toText, trim, remove trailing comma and semicolon
+ *
+ * Additionally, this returns undefined instead of empty string if node is undefined
+ */
+function toTextAndTrim(content?: Node[] | Node | null): string | undefined {
+  const text = toText(content);
+  if (!text) return undefined;
+  return text.replace(/^[\s;,]+/, '').replace(/[\s;,]+$/, '');
+}
+
+function markForDeletion(nodes: (GenericNode | undefined)[]) {
+  nodes.forEach((node) => {
+    if (node) node.type = '__delete__';
+  });
+}
+
+export function processAffiliation(aff: Affiliation): AffiliationFM {
+  const id = aff.id;
+  let ror: string | undefined;
+  let isni: string | undefined;
+  const rorNode = select(`institution-id[institution-id-type=ror]`, aff);
+  if (rorNode) {
+    ror = toTextAndTrim(rorNode);
+  }
+  const isniNode = select(`institution-id[institution-id-type=ISNI]`, aff);
+  if (isniNode) {
+    isni = toTextAndTrim(isniNode);
+  }
+  markForDeletion(selectAll('institution-id', aff));
+  remove(aff, '__delete__');
+  const institutions = selectAll('institution', aff) as GenericNode[];
+  const textAddress = selectAll('addr-line > text', aff) as GenericNode[];
+  const namedContent = selectAll('named-content', aff) as GenericNode[];
+  const departmentNode =
+    institutions.find((inst) => inst['content-type'] === 'dept') ??
+    namedContent.find((content) => content['content-type'] === 'organisation-division');
+  const addressNode = namedContent.find((content) => content['content-type'] === 'street');
+  const cityNode = namedContent.find((content) => content['content-type'] === 'city');
+  const stateNode = namedContent.find((content) => content['content-type'] === 'country-part');
+  const postalCodeNode = namedContent.find((content) => content['content-type'] === 'post-code');
+  const countryNode =
+    select('country', aff) ?? namedContent.find((content) => content['content-type'] === 'country');
+  markForDeletion([
+    ...textAddress,
+    ...namedContent,
+    departmentNode,
+    addressNode,
+    cityNode,
+    stateNode,
+    postalCodeNode,
+    countryNode,
+  ]);
+  remove(aff, '__delete__');
+  const affChildren = aff.children.filter((child) => child.type !== 'label');
+  let institution: string | undefined;
+  if (
+    affChildren.filter((child) => ['text', 'institution-wrap', 'institution'].includes(child.type))
+      .length === affChildren.length
+  ) {
+    institution = toTextAndTrim(affChildren);
+  } else {
+    institution = toTextAndTrim(institutions.find((inst) => inst['content-type'] !== 'dept'));
+  }
+  const addressLines = textAddress
+    .map((line) => toTextAndTrim(line))
+    .filter((line): line is string => !!line);
+  const department = departmentNode
+    ? toTextAndTrim(departmentNode)
+    : addressLines.find((line) => line.toLowerCase().includes('department'));
+  let address = addressNode
+    ? toTextAndTrim(addressNode)
+    : addressLines.find((line) => !line.toLowerCase().includes('department'));
+  if (address && !institution) {
+    institution = address;
+    address = undefined;
+  }
+  const city = toTextAndTrim(cityNode);
+  const state = toTextAndTrim(stateNode);
+  const postal_code = toTextAndTrim(postalCodeNode);
+  const country = toTextAndTrim(countryNode);
+  return { id, ror, isni, department, institution, address, city, state, postal_code, country };
 }
