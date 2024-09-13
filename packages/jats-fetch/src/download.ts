d9a41ec82fd973f1428810febfc094041657f341
Franklin Koch <franklinwkoch@gmail.com>
2024-09-12 23:23:19 -0600

    🪪 Expand pmid fetch functions for dois and multiple pmcids

--- packages/jats-fetch/src/download.ts
+++ packages/jats-fetch/src/download.ts
@@ -102,34 +102,177 @@ type OpenAlexWork = {
   };
 };
 
+export function normalizePMID(session: ISession, pmid: string) {
+  if (pmid.startsWith('https://')) {
+    const idPart = new URL(pmid).pathname.slice(1);
+    session.log.debug(`Extract ${pmid} to ${idPart}`);
+    return idPart;
+  }
+  return pmid;
+}
+
+const IDCONV_URL = 'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/';
+const ESUMMARY_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi';
+
 /**
+ * Convert a single PMID to PMCID
+ *
+ * Returns PMCID
+ *
  * https://www.ncbi.nlm.nih.gov/pmc/tools/id-converter-api/
  */
 export async function convertPMID2PMCID(
   session: ISession,
-  PMID: string,
-  opts: ResolutionOptions,
+  pmid: string,
+  opts?: ResolutionOptions,
 ): Promise<string | undefined> {
-  if (PMID.startsWith('https://')) {
-    const idPart = new URL(PMID).pathname.slice(1);
-    session.log.debug(`Extract ${PMID} to ${idPart}`);
-    return convertPMID2PMCID(session, idPart, opts);
-  }
+  pmid = normalizePMID(session, pmid);
   const toc = tic();
-  const converter = 'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/';
   const resp = await (opts?.fetcher ?? defaultFetcher)(
-    `${converter}?tool=jats-xml&format=json&ids=${PMID}`,
+    `${IDCONV_URL}?tool=jats-xml&format=json&ids=${pmid}`,
     'json',
   );
   if (!resp.ok) {
     // Silently return -- other functions can try!
-    session.log.debug(`Failed to convert PubMedID: ${PMID}`);
+    session.log.debug(`Failed to convert PubMedID: ${pmid}`);
     return;
   }
   const data = await resp.json();
-  const PMCID = data?.records?.[0]?.pmcid;
-  session.log.debug(toc(`Used nih.gov to transform ${PMID} to ${PMCID} in %s.`));
-  return PMCID;
+  const pmcid = data?.records?.[0]?.pmcid;
+  session.log.debug(toc(`Used nih.gov to transform ${pmid} to ${pmcid} in %s.`));
+  return pmcid;
+}
+
+/**
+ * Convert multiple PMIDs to PMCIDs
+ *
+ * Returns a PMID:PMCID lookup dictionary for successful conversions
+ *
+ * https://www.ncbi.nlm.nih.gov/pmc/tools/id-converter-api/
+ */
+export async function convertPMIDs2PMCIDs(
+  session: ISession,
+  pmids: string[],
+  opts?: ResolutionOptions,
+): Promise<Record<string, string> | undefined> {
+  pmids = pmids.map((pmid) => normalizePMID(session, pmid));
+  const toc = tic();
+  const resp = await (opts?.fetcher ?? defaultFetcher)(
+    `${IDCONV_URL}?tool=jats-xml&format=json&ids=${pmids.join(',')}`,
+    'json',
+  );
+  if (!resp.ok) {
+    // Silently return -- other functions can try!
+    session.log.debug(`Failed to convert ${pmids.length} PubMedIDs`);
+    return;
+  }
+  const data = await resp.json();
+  const pmcidEntries = data?.records
+    ?.filter((record: { pmid: string; pmcid?: string }) => record.pmcid)
+    .map((record: { pmid: string; pmcid: string }) => [record.pmid, record.pmcid]);
+  const pmcids = pmcidEntries ? Object.fromEntries(pmcidEntries) : {};
+  session.log.debug(
+    toc(`Used nih.gov to transform ${pmcidEntries.length}/${pmids.length} PMIDs to PMCID in %s.`),
+  );
+  return pmcids;
+}
+
+/**
+ * Query NIH APIs for single DOI from PubMed ID
+ */
+export async function convertPMID2DOI(
+  session: ISession,
+  pmid: string,
+  opts?: ResolutionOptions,
+): Promise<string | undefined> {
+  pmid = normalizePMID(session, pmid);
+  const toc = tic();
+  const idconvResp = await (opts?.fetcher ?? defaultFetcher)(
+    `${IDCONV_URL}?tool=jats-xml&format=json&ids=${pmid}`,
+    'json',
+  );
+  if (idconvResp.ok) {
+    const data = await idconvResp.json();
+    const pmDoi = data?.records?.[0]?.doi;
+    if (pmDoi) {
+      session.log.debug(
+        toc(`Used nih.gov to query ${pmid} for DOI ${pmDoi} in %s. (Tool: idconv)`),
+      );
+      return pmDoi;
+    }
+  }
+  const esummaryResp = await (opts?.fetcher ?? defaultFetcher)(
+    `${ESUMMARY_URL}?db=pubmed&format=json&id=${pmid}`,
+    'json',
+  );
+  if (esummaryResp.ok) {
+    const data = await esummaryResp.json();
+    const pmDoi = data?.result?.[pmid]?.articleids?.find(
+      (articleid: { idtype?: string; value?: string }) => {
+        return articleid.idtype === 'doi';
+      },
+    )?.value;
+    if (pmDoi) {
+      session.log.debug(
+        toc(`Used nih.gov to query ${pmid} for DOI ${pmDoi} in %s. (Tool: esummary)`),
+      );
+      return pmDoi;
+    }
+  }
+  // Silently return -- other functions can try!
+  session.log.debug(`Failed to return DOI from PubMedID: ${pmid}`);
+  return;
+}
+
+/**
+ * Query NIH APIs for multiple DOIs from PubMed IDs
+ */
+export async function convertPMIDs2DOIs(
+  session: ISession,
+  pmids: string[],
+  opts?: ResolutionOptions,
+): Promise<Record<string, string> | undefined> {
+  pmids = [...new Set(pmids.map((pmid) => normalizePMID(session, pmid)))];
+  const pmDois: Record<string, string> = {};
+  const toc = tic();
+  const idconvResp = await (opts?.fetcher ?? defaultFetcher)(
+    `${IDCONV_URL}?tool=jats-xml&format=json&ids=${pmids.join(',')}`,
+    'json',
+  );
+  if (idconvResp.ok) {
+    const data = await idconvResp.json();
+    data?.records?.forEach((record: { pmid: string; doi?: string }) => {
+      if (record.doi) pmDois[record.pmid] = record.doi;
+    });
+    const pmDoiCount = Object.keys(pmDois).length;
+    if (pmDoiCount === pmids.length) {
+      session.log.debug(toc(`Used nih.gov to convert ${pmDoiCount} PMIDs to DOIs in %s.`));
+      return pmDois;
+    }
+  }
+  const esummaryResp = await (opts?.fetcher ?? defaultFetcher)(
+    `${ESUMMARY_URL}?db=pubmed&format=json&id=${pmids.filter((pmid) => !pmDois[pmid]).join(',')}`,
+    'json',
+  );
+  if (esummaryResp.ok) {
+    const data = (await esummaryResp.json()) as {
+      result?: Record<string, { articleids?: { idtype?: string; value?: string }[] }>;
+    };
+    Object.entries(data?.result ?? {}).forEach(([pmid, record]) => {
+      const pmDoi = record.articleids?.find((articleid: { idtype?: string; value?: string }) => {
+        return articleid.idtype === 'doi';
+      })?.value;
+      if (pmDoi) pmDois[pmid] = pmDoi;
+    });
+  }
+  session.log.debug(
+    toc(
+      `Used nih.gov to transform ${Object.keys(pmDois).length}/${
+        pmids.length
+      } PMIDs to PMCID in %s.`,
+    ),
+  );
+  return pmDois;
 }
 
 function pubMedCentralJats(PMCID: string) {
